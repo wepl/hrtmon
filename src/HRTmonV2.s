85483d009199c69ddd2dfa3a9e806c07930d2d99
Bert Jahn <wepl@whdload.de>
2021-01-27 22:07:07 +0100

    added functions printf and _VSNPrintF

--- src/HRTmonV2.s
+++ src/HRTmonV2.s
@@ -9067,6 +9067,301 @@ print_wait	tst.b	no_print
 .nowait		rts
 
 ************************************************************
+;----------------------------------------
+; print format string like printf
+; max 80 chars
+; IN:	A0 = APTR  format string
+;	A1 = APTR  arguments
+; OUT:	-
+
+printf		move.l	a2,-(a7)
+		move.l	a1,a2		; arguments
+		move.l	a0,a1		; format string
+		moveq	#84,d0		; buffer length
+		sub.w	d0,a7
+		subq.l	#3,d0
+		move.l	a7,a0		; buffer
+		bsr	_VSNPrintF
+		move.l	a7,a0
+		bsr	print
+		add.w	#84,a7
+		move.l	(a7)+,a2
+		rts
+
+;----------------------------------------
+; format string like vsnprintf (similar exec.RawDoFmt)
+; IN:	D0 = ULONG length of provided buffer
+;	A0 = APTR  buffer
+;	A1 = APTR  format string
+;	A2 = APTR  arguments
+; OUT:	D0 = ULONG length of created string if buffer length would be unlimited,
+;		   not including the final '\0'
+;	A0 = APTR  points to the terminating null byte of the created string if
+;		   buffer had length > 0
+; D0-D2 = trash
+; D3	= flags 0=minus 1=null 2=long
+; D4	= argument value
+; D5.lw	= precision length (post dot)
+; D5.hw	= field length (pre dot)
+; D6	= counts chars of created string
+; D7	= remaining buffer length
+; A0	= buffer to fill
+; A1	= format string
+; A2	= argument array
+; A3	= trash
+; A4	= temporary buffer for converted numbers
+; (a7)	= 16 byte temporary string buffer
+
+_VSNPrintF	movem.l	d2-d7/a2-a4,-(sp)
+		move.l	d0,d7			;remaining buffer length
+		moveq	#0,d6			;count chars
+		sub.w	#16,a7			;temporary buffer for converted numbers
+		bra	.mainloop
+
+.putcharlast	bsr	.putc
+		add.w	#16,a7
+		move.l	d6,d0
+		movem.l	(sp)+,_MOVEMREGS
+		rts
+
+.putc_term	clr.b	(a0)
+.putc_count	tst.b	d0
+		beq	.putc_rts
+.putc_inc	addq.l	#1,d6
+.putc_rts	rts
+
+.putc		subq.l	#1,d7
+		bmi	.putc_count
+		beq	.putc_term
+		move.b	d0,(a0)+
+		bne	.putc_inc
+		subq.l	#1,a0
+		rts
+
+.mainloop_putc	bsr	.putc
+.mainloop	move.b	(a1)+,d0
+		beq	.putcharlast
+		cmpi.b	#'%',d0
+		bne.b	.mainloop_putc
+		move.l	a7,a4			;a4 = buffer 10 bytes for numbers
+		moveq	#0,d3			;d3 = flags
+		cmpi.b	#'-',(a1)
+		bne.b	.no_minus
+		bset	#0,d3			;bit0=minus
+		addq.l	#1,a1
+.no_minus	cmpi.b	#'0',(a1)
+		bne.b	.no_null
+		bset	#1,d3			;bit1=null
+.no_null	bsr.w	.getnumber
+		move.l	d0,d5
+		swap	d5			;d5.hw=field width (pre dot)
+		cmpi.b	#'.',(a1)
+		bne.b	.no_dot
+		addq.l	#1,a1
+		bsr.w	.getnumber
+		move.w	d0,d5			;d5.lw=precision (post dot)
+.no_dot		cmpi.b	#'l',(a1)
+		bne.b	.no_l
+		bset	#2,d3			;bit2=l
+		addq.l	#1,a1
+
+.no_l		move.b	(a1)+,d0
+		cmpi.b	#'d',d0
+		beq.b	.d
+		cmpi.b	#'D',d0
+		bne.b	.notd
+.d		bsr.b	.getarg_d4
+		bsr.w	.putints
+		bra.w	.putbuffer
+
+.notd		cmpi.b	#'x',d0
+		beq.b	.x
+		cmpi.b	#'X',d0
+		bne.b	.notx
+.x		bsr.b	.getarg_d4
+.xput		bsr.w	.putintx
+		bra.b	.putbuffer
+
+.getarg_d4	btst	#2,d3			;long?
+		bne.b	.getargl_d4
+		move.w	(a2)+,d4
+		ext.l	d4
+		rts
+
+.getargl_d4	move.l	(a2)+,d4
+		rts
+
+.notx		cmpi.b	#'s',d0
+		bne.b	.nots
+		bsr.b	.getargl_d4
+		beq	.mainloop
+		movea.l	d4,a4
+		bra.b	.putbuffera4
+
+.nots		cmpi.b	#'B',d0			;BPTR
+		bne.b	.notbptr
+		bsr.b	.getargl_d4
+		lsl.l	#2,d4			;BPTR -> APTR
+		bset	#2,d3			;flag l
+		bra	.xput
+
+.notbptr	cmpi.b	#'b',d0			;BSTR
+		bne.b	.notbstr
+		bsr.b	.getargl_d4
+		beq	.mainloop
+		lsl.l	#2,d4			;BSTR -> APTR
+		movea.l	d4,a4
+		moveq	#0,d2
+		move.b	(a4)+,d2
+		beq	.mainloop
+		tst.b	(-1,a4,d2.w)
+		bne.b	.putbufferd2
+		subq.w	#1,d2
+		bra.b	.putbufferd2
+
+.notbstr	cmpi.b	#'u',d0
+		beq.b	.u
+		cmpi.b	#'U',d0
+		bne.b	.notu
+.u		bsr.b	.getarg_d4
+		bsr.w	.putintu
+		bra.b	.putbuffer
+
+.notu		cmpi.b	#'c',d0
+		bne	.mainloop_putc
+		bsr.b	.getarg_d4
+		move.b	d4,(a4)+
+.putbuffer	clr.b	(a4)		;terminate string
+		move.l	a7,a4		;rewind
+.putbuffera4	movea.l	a4,a3
+		moveq	#-1,d2
+.lenbufloop	tst.b	(a3)+
+		dbeq	d2,.lenbufloop
+		not.l	d2		;d2 = buffer length
+.putbufferd2	tst.w	d5		;precision (post dot)
+		beq.b	.noprec
+		cmp.w	d5,d2
+		bhi.b	.setprelen
+.noprec		move.w	d2,d5		;precision = buffer length
+.setprelen	move.l	d5,d0
+		swap	d5		;field width
+		sub.w	d0,d5		;d5 = field width - precision = align length
+		bpl.b	.prelenok
+		clr.w	d5
+.prelenok	swap	d5
+		btst	#0,d3		;flag minus? (left align)
+		bne.b	.putbuffer_cin
+		bsr.b	.align
+		bra.b	.putbuffer_cin
+
+.putbuffer_copy	move.b	(a4)+,d0
+		bsr	.putc
+.putbuffer_cin	dbra	d5,.putbuffer_copy
+		btst	#0,d3		;flag minus? (left align)
+		beq	.mainloop
+		bsr.b	.align
+		bra	.mainloop
+
+.align		move.l	d5,d1
+		swap	d1
+		moveq	#' ',d2
+		btst	#1,d3		;flag 0?
+		beq.b	.align_copyin
+		cmpi.b	#'-',(a4)
+		bne.b	.align_not_neg
+		move.b	(a4)+,d0	;'-'
+		subq.w	#1,d5
+		bsr	.putc
+.align_not_neg	moveq	#'0',d2
+		bra.b	.align_copyin
+
+.align_copy	move.b	d2,d0
+		bsr	.putc
+.align_copyin	dbra	d1,.align_copy
+		rts
+
+.getnumber	moveq	#0,d0
+		moveq	#0,d2
+.getnumber_loop	move.b	(a1)+,d2
+		cmpi.b	#'0',d2
+		bcs.b	.getnumber_end
+		cmpi.b	#'9',d2
+		bhi.b	.getnumber_end
+		add.l	d0,d0
+		move.l	d0,d1
+		add.l	d0,d0
+		add.l	d0,d0
+		add.l	d1,d0
+		sub.b	#'0',d2
+		add.l	d2,d0
+		bra.b	.getnumber_loop
+
+.getnumber_end	subq.l	#1,a1
+		rts
+
+.putints	tst.l	d4
+		bpl.b	.putintu
+		move.b	#'-',(a4)+
+		neg.l	d4
+.putintu	moveq	#'0',d0
+		lea	(.dectab,pc),a3
+.putint_loop	move.l	(a3)+,d1
+		beq.b	.putint_end
+		moveq	#'/',d2
+.putint_lp	addq.l	#1,d2
+		sub.l	d1,d4
+		bcc.b	.putint_lp
+		add.l	d1,d4
+		cmp.l	d0,d2
+		beq.b	.putint_loop
+		moveq	#0,d0
+		move.b	d2,(a4)+
+		bra.b	.putint_loop
+
+.putint_end	moveq	#'0',d0
+		add.b	d0,d4
+		move.b	d4,(a4)+
+		rts
+
+.putintx	tst.l	d4
+		beq.b	.putint_end
+		clr.w	d1			;d1 flag already char written
+		btst	#2,d3			;flag l
+		bne.b	.putintx_l
+		moveq	#3,d2
+		swap	d4
+		bra.b	.putintx_loop
+
+.putintx_l	moveq	#7,d2
+.putintx_loop	rol.l	#4,d4
+		move.b	d4,d0
+		and.b	#15,d0
+		bne.b	.putintx_write
+		tst.w	d1
+		beq.b	.putintx_skip
+.putintx_write	moveq	#-1,d1
+		cmp.b	#9,d0
+		bhi.b	.putintx_alpha
+		add.b	#'0',d0
+		bra.b	.putintx_set
+
+.putintx_alpha	add.b	#'7',d0
+.putintx_set	move.b	d0,(a4)+
+.putintx_skip	dbra	d2,.putintx_loop
+		rts
+
+.dectab		dc.l	1000000000
+		dc.l	100000000
+		dc.l	10000000
+		dc.l	1000000
+		dc.l	100000
+		dc.l	10000
+		dc.l	1000
+		dc.l	100
+		dc.l	10
+		dc.l	0
+
+************************************************************
 
 no_sc_clr	dc.b 0
 		even
